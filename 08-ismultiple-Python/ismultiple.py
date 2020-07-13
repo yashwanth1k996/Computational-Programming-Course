2b5c45be52871dd19e23db4965415d81c1f9efcf
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-13 20:40:58 +0530

    is multiple complete

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,14 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if(m == 0):
+    		return True
+
+	if(n == 0):
+    		return False
+			
+	if(m % n == 0):
+    		return True
+
+	else:
+    		return False
