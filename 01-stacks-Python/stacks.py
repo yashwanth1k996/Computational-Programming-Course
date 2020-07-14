dad8603f830a684cd24d42fc28208c8e6ed09b31
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-14 15:11:00 +0530

    Jul 14  15:11:00

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,11 +26,14 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        new_element.next = self.head
+        self.head = new_element
 
     def delete_first(self):
-        "Delete the first (head) element in the LinkedList as return it"
-        pass
+        "Delete the first (head) element in the LinkedList and return it"
+        val = self.head.value
+        self.head = self.head.next
+        return val
 
 class stack(object):
     def __init__(self,top=None):
