493f4ec38186c50a8a29b17549e545815ab4bedb
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-15 00:56:05 +0530

    done

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -12,7 +12,10 @@ def fun_set_kth_digit(n, k, d):
 			while(n > 0):
 					list1.append(str(n%10))
 					n = n//10
-			list1[k] = str(d)
+			if(k >= len(list1)):
+    				list1.append(str(d))
+			else:
+					list1[k] = str(d)
 			list1.reverse()
 			new = "".join(list1)
 			return int(new)
@@ -21,10 +24,13 @@ def fun_set_kth_digit(n, k, d):
 		while(n > 0):
 			list1.append(str(n%10))
 			n = n//10
-		list1[k] = str(d)
+		if(k >= len(list1)):
+    			list1.append(str(d))
+		else:
+				list1[k] = str(d)
 		list1.reverse()
 		new = "".join(list1)
 		new = "-"+ new
 		return int(new)
 
-print(fun_set_kth_digit(468, 0, 1))
+print(fun_set_kth_digit(-468, 3, 1))
