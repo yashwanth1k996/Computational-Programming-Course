986ea78ac2c3ee5efa260f3e39a1d34ff38b7639
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-14 23:34:56 +0530

    Jul 14  23:34:55

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -3,10 +3,13 @@
 # value in the given row and column of Pascal's 
 # Triangle where the triangle starts at row 0, and 
 # each row starts at column 0. If either row or col 
-# are not legal values, return None, instead of crashing. 
-
-
+# are not legal values, return None, instead of crashing.
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+    if((row+1)==1 or (col+1)==1 or row==col):
+    		return 1
+	
+    else:
+    		return fun_pascaltrianglevalue(row-1,col-1)+ fun_pascaltrianglevalue(row-1,col)
+	
