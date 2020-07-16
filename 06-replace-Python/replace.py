b832027eec4dfbc80b2b66d3dff4353391baf2fc
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-16 21:51:13 +0530

    done

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -11,12 +11,19 @@ def fun_replace(s1, s2, s3):
 	s1len = len(s1)
 	for i in range(0, s1len-s2len+1):
 		if(s1[i:len(s2)+i] == s2):
-			s = s1[:i]+s3+s1[(i+len(s2)) :]
-			flag = True
+			if(flag == True):
+				s = s[:i]+s3+s1[(i+len(s2)) :]
+				flag = True
+			else:
+				s = s1[:i]+s3+s1[(i+len(s2)) :]
+				s1 = s
+				flag = True
 	if(flag == True):
 		return s
 	else:
 		return s1
 
-print(fun_replace("hellrldowo23ufn348hf oincodnrld123", "rld", "     "))
+# print(fun_replace("hellrldowo23ufn348hf oincodnrld123", "rld", "     "))
 
+"hell     owo23ufn348hf oincodn     123"
+"hell     owo23ufn348hf oincodn     123"
