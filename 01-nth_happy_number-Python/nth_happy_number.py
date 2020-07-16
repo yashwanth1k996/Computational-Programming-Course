cb6aaf3b40fd65ffeff6acf07dbae7982b8b5de6
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-16 14:24:43 +0530

    Jul 16  14:24:43

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -13,28 +13,32 @@
 def newNumber(n):
     sum = 0
     while n > 0:
-        sum += (n%10)
+        sum += (n%10)**2
         n = n//10
     return sum
 
 def happuNumber(n):
     list1 = []
-    if(n == 1):
-        return True
-    if(n in list1):
-        return False
-    list1.append(n)
-    n = newNumber(n)
+    while True:
+        if(n == 1):
+            return True
+        if(n in list1):
+            return False
+        list1.append(n)
+        n = newNumber(n)
 
 def fun_nth_happy_number(n):
+    if(n == 0):
+        return 1
     count = 0
-    val = 1
-    while(count == n):
+    val = 2
+    while(count != n):
         if(happuNumber(val)):
             count += 1
+            print(val)
             if(count == n):
                 break
-        val +=1
+        val += 1
     return val
 
-	
+print(fun_nth_happy_number(3))	
