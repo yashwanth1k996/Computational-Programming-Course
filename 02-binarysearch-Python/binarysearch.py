382d130ed32e7515cebfb9f27e32a0b9996b83db
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-17 21:35:29 +0530

    Jul 17  21:35:29

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -12,7 +12,23 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    min = input_array[0]
-    max = input_array[len(input_array)]
+    min = 0
+    max = len(input_array)-1
     mid = min+max // 2
-    
+    while(mid < len(input_array)):
+        # print(input_array[mid])
+        print(min, max)
+        print(mid)
+        if(value > input_array[mid]):
+            min = mid
+            mid = min+max // 2
+            print(mid)
+        elif(value < input_array[mid]):
+            max = mid
+            mid = max+min // 2
+        else:
+            return mid
+    return -1
+
+
+print(binary_search([1,3,9,11,15,19,29], 15))
