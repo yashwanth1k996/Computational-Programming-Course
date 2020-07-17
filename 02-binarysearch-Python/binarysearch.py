be3da5e91f25e43c2833089770001a4a1ac225e0
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-17 21:55:58 +0530

    Jul 17  21:55:58

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -4,31 +4,40 @@ using loops.
 Your function should take two inputs:
 a Python list to search through, and the value
 you're searching for.
-Assume the list only has distinct elements,
+Assume the list only has distinct valuements,
 meaning there are no repeated values, and 
 elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
-def binary_search(input_array, value):
-    # Your code goes here
-    min = 0
-    max = len(input_array)-1
-    mid = min+max // 2
-    while(mid < len(input_array)):
-        # print(input_array[mid])
-        print(min, max)
-        print(mid)
-        if(value > input_array[mid]):
-            min = mid
-            mid = min+max // 2
-            print(mid)
-        elif(value < input_array[mid]):
-            max = mid
-            mid = max+min // 2
+def binary_search(arr,ele):
+    
+    # First and last index values
+    first = 0
+    last = len(arr) - 1
+    
+    found = False
+    
+    
+    while first <= last and not found:
+        
+        mid = (first+last)//2 # or // for Python 3
+        
+        # Match found
+        if arr[mid] == ele:
+            found = True
+        
+        # Set new midpoints up or down depending on comparison
         else:
-            return mid
+            # Set down
+            if ele < arr[mid]:
+                last = mid -1
+            # Set up 
+            else:
+                first = mid + 1
+                
     return -1
 
 
-print(binary_search([1,3,9,11,15,19,29], 15))
+print(binary_search([1,3,9,11,15,19,29], 24))
+
