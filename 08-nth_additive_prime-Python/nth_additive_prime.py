bfe6a8167751f5524c94a65071eb22e563ec84e0
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-18 17:48:01 +0530

    done

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,9 +6,42 @@
 
 
 
+def digsum(val):
+	sum = 0
+	while(val > 0):
+		sum += val%10
+		val = val // 10
+	return sum
+
+def isprime(val):
+	count = 0
+	for i in range(2, val//2+1):
+		if(val % i == 0):
+			count += 1
+	if(count > 0):
+		return False
+	else:
+		return True   		
+
+
+def isadditiveprime(val):
+	if(isprime(val)):
+		sum = digsum(val)
+		if(isprime(sum)):
+			return True
+		else:
+			return False
+	else:
+		return False
+
 def fun_nth_additive_prime(n):
 	if(n == 0):
 		return 2
+	val = 2
+	count = 0
 	while(count < n):
+		val += 1
 		if(isadditiveprime(val)):
 			count += 1
+			
+	return val
