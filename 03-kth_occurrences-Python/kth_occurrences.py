cf46c291e2638743432d847f768285f3bebe33c3
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-19 01:17:30 +0530

    Jul 19  01:17:29

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,7 @@
 
 def fun_kth_occurrences(s, n):
 	d = {}
+	s = s.strip()
 	for i in s:
 		if(i in d):
 			d[i] += 1
@@ -14,4 +15,4 @@ def fun_kth_occurrences(s, n):
 		if(d[j]-1 == n):
 			return j
 
-print(fun_kth_occurrences("helllo woorld", 2))
+print(fun_kth_occurrences("    h  ", 2))
