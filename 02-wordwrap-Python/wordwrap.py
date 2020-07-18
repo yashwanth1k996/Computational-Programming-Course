e75192a67139542ff693c01ec14512b8971596dc
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-19 00:55:24 +0530

    Jul 19  00:55:24

--- 02-wordwrap-Python/wordwrap.py
+++ 02-wordwrap-Python/wordwrap.py
@@ -18,9 +18,14 @@
 
 def fun_wordwrap(s, n):
 	s = s.strip(" ")
-	new = ""
+	new = """\ """
 	for i in range(0,len(s)-n, n):
-		new += """\ """ + s[i:i+4] + """\n"""
+		for j in range(i,i+n):
+			if(s[j] == " "):
+				new += "-"
+			else:
+				new += s[j]
+				new += """\n """
 	return new
 
 
