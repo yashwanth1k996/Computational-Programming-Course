cb4247ba1a4a37a712b69db9c9a974bb2798b7cf
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-20 11:12:45 +0530

    Jul 20  11:12:45

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -11,4 +11,24 @@
 
 def shortenlongruns(L, k):
 	# Your code goes here
-	pass
+	list1 = []
+	count = 0
+	val = 0
+	for i in range(0, len(L)):
+		if(i == 0):
+			list1.append(i)
+			val = i
+			count = 1
+		else:
+			if(i == val):
+				count += 1
+				if(count <= k):
+					list1.append(i)
+			else:
+				val = i
+				list1.append(i)
+				count = 1
+	return list1
+		
+    			
+
