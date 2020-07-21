3e6f93aa05287a45087bebbf49f473f6a21505a0
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-21 19:05:19 +0530

    Jul 21  19:05:19

--- 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
+++ 05-recursion_binarysearchvales-Python/recursion_binarysearchvalues.py
@@ -17,7 +17,23 @@
 #     assert(binarySearchValues(L, v) == [(2,'f'), (0,'a'), (1,'c')])
 # Hint: Do not slice the list L, but rather adjust the indexes into L. 
 
+def recbinser(l, v, list1, low, high):
+	mid = (low + high)//2
+	if(l[mid] == v):
+		return list1.append(mid, l[mid])
+	else:
+		if(v > l[mid]):
+			low = mid+1
+		else:
+			high = mid-1
+			return recbinser(l,v, list1.append(mid, l[mid]), low, high)
+
+
+
+
 def recursion_binarysearchvalues(L, v):
 	# Your codes goes here
-	pass
+	list1 = []
+	list1 = recbinser(L, v, list1, 0, len(L)-1)
+	return list1
 	
