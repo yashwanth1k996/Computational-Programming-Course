fb90516155ddcdc9074b4f82d34049ce2aa2b5c7
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-21 18:48:39 +0530

    Jul 21  18:48:38

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -3,6 +3,25 @@
 # positive powers of 3 up to and including n. As an example, powersOf3ToN(10.5) returns [1, 3, 9]. If no such powers 
 # of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
 
+
+def sol(n, list1, comp):
+	if(comp > n):
+		return list1
+	else:
+		if(comp**3 < n):
+			list1.append(comp)
+			return sol(n, list1, comp+1)
+		else:
+			return sol(n, list1, comp+1)
+
+
 def recursion_powersof3ton(n):
 	# Your code goes here
-	pass
+	if(abs(n) != n):
+		return None
+	else:
+		list1 = []
+		list1 = sol(n, list1, 1)
+	if(list1 == []):
+		return None
+	return list1
