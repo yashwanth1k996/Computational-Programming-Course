3181cf70ceda009e3bab9c72f84893100e0bc360
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-21 14:58:44 +0530

    Jul 21  14:58:44

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -3,15 +3,25 @@
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
 
+import numpy as np
 
 def fun_matrixmultiply(m1, m2):
     rows1 = len(m1)
     rows2 = len(m2)
     col1 = len(m1[0])
     col2 = len(m2[0])
-    if(col1 != rows2):
-        return None 
-
+    if(rows1 != col2):
+        return None
+    mat = []
+    for i in range(0, rows1):
+        matin = []
+        for j in range(0, col2):
+            matin.append(0)
+        mat.append(matin)
+    for i in range(0, len(m1)):
+        for j in range(0, len(m2[0])):
+            for k in range(0, len(m2)):
+                mat[i][j] += m1[i][k] * m2[k][j]
 
 
 
