9b1ce512dfb95e0e64db0835d7562f277cbd6b8c
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-23 19:44:43 +0530

    Jul 23  19:44:43

--- 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
+++ 02-nearestkaprekarnumber-Python/nearestkaprekarnumber.py
@@ -9,8 +9,45 @@
 # Hint: one way to solve this is to start at n and grow in each direction until you find a Kaprekar number.
 
 
+def isequal(val, n):
+    for i in range(1, len(val)):
+        if((int(val[:i]) + int(val[i:])) == n and int(val[i:]) != 0):
+            return True
+    return False
 
-import math
+
+def iskaprekar(n):
+    val = n**2
+    vallen = len(str(val))
+    mid = vallen//2
+    strval = str(val)
+    if(vallen == 1):
+        if(val == n):
+            return True
+        else:
+            return False
+    if(isequal(strval, n)):
+        return True
+    else:
+        return  False
+
 
 def fun_nearestkaprekarnumber(n):
-    return 1
+    vallow = 0
+    valhigh = 0
+    for i in range(n, 0, -1):
+        if(iskaprekar(i)):
+            vallow = i
+            break
+    j = n+1
+    while(j > n):
+        if(iskaprekar(j)):
+            valhigh = j
+            break
+        j += 1
+    if((n-vallow) > (valhigh-n)):
+        return valhigh
+    else:
+        return vallow
+
+print(fun_nearestkaprekarnumber(49))
