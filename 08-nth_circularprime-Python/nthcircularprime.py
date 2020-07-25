3c9903bd53898a0b6fad2b72949354cc26c39899
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-07-25 19:26:35 +0530

    pending

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -5,36 +5,43 @@
 # 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 197... To see why 197 is a Circular prime, 
 # note that 197 is prime, as is 971 (rotated left), as is 719 (rotated left again).
 
-def reverseset(x):
-	list1 = []
-	x = str(x)
-	for i in range(0, len(x)):
-		list1.append(int(x[i:] + x[:i]))
-	return list1
-
-
 def isprime(n):
 	for i in range(2, n//2+1):
 		if(n%i == 0):
 			return False
 	return True
-	
+
+
+def reverseset(x):
+	x = str(x)
+	if(x != '2'):
+		for i in x:
+			if(int(i)%2 == 0):
+				return False
+	for i in range(0, len(x)):
+		if(not isprime(int(x[i:] + x[:i]))):
+			return False
+	return True
 
 
 
 def nthcircularprime(n):
 	# Your code goes here
 	count = -1
+	val = 1
 	while(count < n):
+		if(val == 11):
+			val += 2
 		if(isprime(val)):
-			list1 = reverseset(val)
-			flag = True
-			for i in list1:
-				if(not isprime(i)):
-					flag = False
-					break
-			if(flag):
+			if(reverseset(val)):
 				count += 1
 				if(count == n):
 					return val
+		if(val > 2):
+			val += 2
+		else:
 			val += 1
+			
+			
+
+print(nthcircularprime(39))
